367fc1f90719d79a15c541e8fb70ce7da46b639f
Akash Trehan <akash.trehan123@gmail.com>
2017-11-24 00:58:50 +0530

    included nbtree.h; make working for smsort

--- src/backend/access/smerge/smsort.c
+++ src/backend/access/smerge/smsort.c
@@ -1,5 +1,56 @@
 #include "postgres.h"
 #include "access/smerge.h"
+#include "access/nbtree.h"
+
+/*
+ * Status record for spooling/sorting phase.  (Note we may have two of
+ * these due to the special requirements for uniqueness-checking with
+ * dead tuples.)
+ */
+struct BTSpool
+{
+    Tuplesortstate *sortstate;  /* state data for tuplesort.c */
+    Relation    heap;
+    Relation    index;
+    bool        isunique;
+};
+
+/*
+ * Status record for a btree page being built.  We have one of these
+ * for each active tree level.
+ *
+ * The reason we need to store a copy of the minimum key is that we'll
+ * need to propagate it to the parent node when this page is linked
+ * into its parent.  However, if the page is not a leaf page, the first
+ * entry on the page doesn't need to contain a key, so we will not have
+ * stored the key itself on the page.  (You might think we could skip
+ * copying the minimum key on leaf pages, but actually we must have a
+ * writable copy anyway because we'll poke the page's address into it
+ * before passing it up to the parent...)
+ */
+typedef struct BTPageState
+{
+    Page        btps_page;      /* workspace for page building */
+    BlockNumber btps_blkno;     /* block # to write this page at */
+    IndexTuple  btps_minkey;    /* copy of minimum key (first item) on page */
+    OffsetNumber btps_lastoff;  /* last item offset loaded */
+    uint32      btps_level;     /* tree level (0 = leaf) */
+    Size        btps_full;      /* "full" if less than this much free space */
+    struct BTPageState *btps_next;      /* link to parent level, if any */
+} BTPageState;
+
+/*
+ * Overall status record for index writing phase.
+ */
+typedef struct BTWriteState
+{
+    Relation    heap;
+    Relation    index;
+    bool        btws_use_wal;   /* dump pages to WAL? */
+    BlockNumber btws_pages_alloced;     /* # pages allocated */
+    BlockNumber btws_pages_written;     /* # pages written out */
+    Page        btws_zeropage;  /* workspace for filling zeroes */
+} BTWriteState;
 
 /*
  * Read tuples in correct sort order from tuplesort, and load them into
@@ -93,6 +144,8 @@ _sm_merge_k(BTWriteState *wstate, BTSpool **btspool, int k)
             for(; j < k; j++) {
                 if(is_empty[j])
                     continue;
+
+                itup2 = itup[j];
 
                 /* Compare two tuples */
                 for (i = 1; i <= keysz; i++)
